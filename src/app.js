7105eedc1ecaa05e05c4f4a33d95ca1968bd3d1d
ofy-naveen <jangirnaveen2212@gmail.com>
2024-09-26 10:39:24 +0530

    app configurations done

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ import cors from 'cors'
 
 const app = express()
 
-
-
 app.use(cors(
     {
         origin : process.env.CORS_ORIGIN,
         credentials : true
     }
 ))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 
 export default app
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,6 @@
 
 
+
 const asyncHandler = (requestHandler) =>{
     return (req,res,next) => {
         Promise.resolve(requestHandler(req,res,next)).
