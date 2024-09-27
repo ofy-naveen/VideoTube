7f1e623f3ccdb48af4982ba4165ad7a969b851ce
ofy-naveen <jangirnaveen2212@gmail.com>
2024-09-27 18:35:35 +0530

    bug fixed, server is running

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,5 @@
-import ApiError from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler.js ";
 import ApiError from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js ";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
